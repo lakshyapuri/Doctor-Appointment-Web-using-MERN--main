2f8c13231de51b62c72b038d4afbcd73718741ac
lakshyapuri <lakshyapuri2004@gmail.com>
2025-03-06 23:28:20 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import doctorRoutes from "./routes/doctorRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware for CORS and JSON parsing
+const allowedOrigins = ["http://localhost:5176"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/doctor", doctorRoutes);
+app.use("/api/reviews", reviewRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
